1c4432ca5b79b4966902d505f3cc73db9cdb9037
Supran Sengupta <supransengupta@gmail.com>
2019-09-02 08:18:55 +0530

    added redis caching

---
+++ services/cache.js
@@ -0,0 +1,52 @@
+const redis = require('redis');
+
+const client = redis.createClient('redis://localhost:6379');
+
+const util = require('util');
+
+const mongoose = require('mongoose');
+
+const exec = mongoose.Query.prototype.exec;
+
+client.hget = util.promisify(client.hget);
+
+mongoose.Query.prototype.cache = function(options = {}) {
+    this.useCache = true; // To set that caching will be used in exec
+    this.hashKey = JSON.stringify(options.key || "");
+
+    return this; // In order to support chaining of functions.
+}
+
+mongoose.Query.prototype.exec = async function() {
+    if(!this.useCache) {
+        return exec.apply(this,arguments);
+    }
+
+    const key = JSON.stringify({...this.getQuery() , collection: this.mongooseCollection.name});
+
+    const result = await client.hget(this.hashKey,key);
+
+    if(result) {
+        const doc = JSON.parse(result);
+
+        return (Array.isArray(doc)) 
+            ?  doc.map(d => new this.model(d))
+            :  new this.model(doc);
+    }
+    
+
+    const data = await exec.apply(this,arguments);
+
+    if(data) {
+        client.hset(this.hashKey, key , JSON.stringify(data));
+    }
+
+    return data;
+}
+
+
+module.exports = {
+    clearCache: function (hashKey) {
+            client.del(JSON.stringify(hashKey));
+    }
+}
